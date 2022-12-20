00e9b45b124c5ca000e1f2009a3c8fa730c5e521
swajeet01 <swajeetg768@gmail.com>
2022-12-20 11:37:43 +0530

    Add diclarations to bricks.h

---
+++ src/bricks.h
@@ -0,0 +1,32 @@
+#ifndef BRICKS_INCL
+#define BRICKS_INCL
+
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_primitives.h>
+
+#include "display.h"
+
+#define BRICK_W (BUFFER_W * DISP_SCALE)
+#define BRICK_H (BUFFER_H * DISP_SCALE)
+
+#define BRICK_R 4
+#define BRICK_C (BUFFER_W / BRICK_W)
+
+#define BRICK_START_X 50
+#define BRICK_START_Y 0
+
+typedef struct {
+    int x;
+    int y;
+    bool destroyed;
+    struct {
+        float r;
+        float g;
+        float b;
+    } color;
+} Bricks;
+
+void init_bricks(Bricks bricks[BRICK_R][BRICK_C]);
+void draw_bricks(Bricks bricks[BRICK_R][BRICK_C]);
+
+#endif
