faf436d3e91d778017b96fe154ab50a0dbf872b2
swajeet01 <swajeetg768@gmail.com>
2022-12-20 13:51:44 +0530

    Add pad files.

---
+++ src/pad.c
@@ -0,0 +1,10 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_primitives.h>
+
+#include "pad.h"
+
+void draw_pad(int pad_x) {
+    al_draw_filled_rectangle(pad_x, BUFFER_H - PAD_H, pad_x + PAD_W, BUFFER_H,
+            al_map_rgb_f(1, 1, 1)
+    );
+}
---
+++ src/pad.h
@@ -0,0 +1,14 @@
+#ifndef BRICKS_INCL
+#define BRICKS_INCL
+
+#define PAD_W 75
+#define PAD_H 8
+
+typedef struct {
+    int x;
+    int y;
+} Pad;
+
+void draw_pad(int pad_x);
+
+#endif
